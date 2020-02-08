hcorion/llvm-project

commit e19ae312c4b189dba5d94a656780352e2bcab44f

[ObjC][ARC] Delete ARC runtime calls that take inert phi values
This improves on the following patch, which removed ARC runtime calls
taking inert global variables:

https://reviews.llvm.org/D62433

rdar://problem/59137105

llvm/lib/Transforms/ObjCARC/ObjCARCOpts.cpp
@@ -877,23 +877,43 @@ void ObjCARCOpt::OptimizeIndividualCalls(Function &F) {
   optimizeDelayedAutoreleaseRV();
 }
 
+/// This function returns true if the value is inert. An ObjC ARC runtime call
+/// taking an inert operand can be safely deleted.
+static bool isInertARCValue(Value *V) {
+  V = V->stripPointerCasts();
+
+  if (IsNullOrUndef(V))
+    return true;
+
+  // See if this is a global attribute annotated with an 'objc_arc_inert'.
+  if (auto *GV = dyn_cast<GlobalVariable>(V))
+    if (GV->hasAttribute("objc_arc_inert"))
+      return true;
+
+  if (auto PN = dyn_cast<PHINode>(V)) {
+    // Look through phis's operands.
+    for (Value *Opnd : PN->incoming_values())
+      if (!isInertARCValue(Opnd))
+        return false;
+    return true;
+  }
+
+  return false;
+}
+
 void ObjCARCOpt::OptimizeIndividualCallImpl(
     Function &F, DenseMap<BasicBlock *, ColorVector> &BlockColors,
     Instruction *Inst, ARCInstKind Class, const Value *Arg) {
   LLVM_DEBUG(dbgs() << "Visiting: Class: " << Class << "; " << *Inst << "\n");
 
-  // Some of the ARC calls can be deleted if their arguments are global
-  // variables that are inert in ARC.
-  if (IsNoopOnGlobal(Class)) {
-    Value *Opnd = Inst->getOperand(0);
-    if (auto *GV = dyn_cast<GlobalVariable>(Opnd->stripPointerCasts()))
-      if (GV->hasAttribute("objc_arc_inert")) {
-        if (!Inst->getType()->isVoidTy())
-          Inst->replaceAllUsesWith(Opnd);
-        Inst->eraseFromParent();
-        return;
-      }
-  }
+  // We can delete this call if it takes an inert value.
+  if (IsNoopOnGlobal(Class))
+    if (isInertARCValue(Inst->getOperand(0))) {
+      if (!Inst->getType()->isVoidTy())
+        Inst->replaceAllUsesWith(Inst->getOperand(0));
+      Inst->eraseFromParent();
+      return;
+    }
 
   switch (Class) {
   default:

llvm/test/Transforms/ObjCARC/inert-global.ll
@@ -54,6 +54,39 @@ define internal void @__globalBlock_block_invoke(i8* nocapture readnone) {
   ret void
 }
 
+; CHECK: define %[[V0:.*]]* @test_conditional0(
+; CHECK: %[[PHI0:.*]] = phi %[[V0]]* [ bitcast (%struct.__NSConstantString_tag* @_unnamed_cfstring_ to %[[V0]]*), %{{.*}} ], [ null, %{{.*}} ]
+
+; CHECK: %[[PHI1:.*]] = phi %[[V0]]* [ bitcast (%struct.__NSConstantString_tag* @_unnamed_cfstring_ to %[[V0]]*), %{{.*}} ], [ %[[PHI0]], %{{.*}} ]
+; CHECK-NEXT: %[[PHI2:.*]] = phi %[[V0]]* [ bitcast (%struct.__NSConstantString_tag* @_unnamed_cfstring_ to %[[V0]]*), %{{.*}} ], [ %{{.*}}, %{{.*}} ]
+; CHECK-NEXT: %[[V2:.*]] = bitcast %[[V0]]* %[[PHI1]] to i8*
+; CHECK-NEXT: %[[V4:.*]] = bitcast %[[V0]]* %[[PHI2]] to i8*
+; CHECK-NEXT: %[[V5:.*]] = tail call i8* @llvm.objc.autoreleaseReturnValue(i8* %[[V4]])
+; CHECK-NEXT: ret %[[V0]]* %[[PHI2]]
+
+define %0* @test_conditional0(i32 %i, %0* %b) {
+entry:
+  %v0 = icmp eq i32 %i, 1
+  br i1 %v0, label %bb2, label %bb1
+
+bb1:
+  %v1 = icmp eq i32 %i, 2
+  br i1 %v1, label %bb2, label %return
+
+bb2:
+  %phi0 = phi %0* [ bitcast (%struct.__NSConstantString_tag* @_unnamed_cfstring_ to %0*), %entry ], [ null, %bb1 ]
+  br label %return
+
+return:
+  %phi1 = phi %0* [ bitcast (%struct.__NSConstantString_tag* @_unnamed_cfstring_ to %0*), %bb1 ], [ %phi0, %bb2 ]
+  %phi2 = phi %0* [ bitcast (%struct.__NSConstantString_tag* @_unnamed_cfstring_ to %0*), %bb1 ], [ %b, %bb2 ]
+  %v2 = bitcast %0* %phi1 to i8*
+  %v3 = tail call i8* @llvm.objc.autoreleaseReturnValue(i8* %v2)
+  %v4 = bitcast %0* %phi2 to i8*
+  %v5 = tail call i8* @llvm.objc.autoreleaseReturnValue(i8* %v4)
+  ret %0* %phi2
+}
+
 declare void @foo()
 
 declare i8* @llvm.objc.retain(i8*) local_unnamed_addr

commit 0acd770a4e1e1cceb720751b992ed62de5e59b24

[ObjC][ARC] Keep track of phis that have been discovered to avoid an infinite loop
This fixes a bug introduced in e19ae312c4b189dba5d94a656780352e2bcab44f.

rdar://problem/59137105

llvm/lib/Transforms/ObjCARC/ObjCARCOpts.cpp
@@ -879,7 +879,7 @@ void ObjCARCOpt::OptimizeIndividualCalls(Function &F) {
 
 /// This function returns true if the value is inert. An ObjC ARC runtime call
 /// taking an inert operand can be safely deleted.
-static bool isInertARCValue(Value *V) {
+static bool isInertARCValue(Value *V, SmallPtrSet<Value *, 1> &VisitedPhis) {
   V = V->stripPointerCasts();
 
   if (IsNullOrUndef(V))
@@ -891,9 +891,12 @@ static bool isInertARCValue(Value *V) {
       return true;
 
   if (auto PN = dyn_cast<PHINode>(V)) {
+    // Ignore this phi if it has already been discovered.
+    if (!VisitedPhis.insert(PN).second)
+      return true;
     // Look through phis's operands.
     for (Value *Opnd : PN->incoming_values())
-      if (!isInertARCValue(Opnd))
+      if (!isInertARCValue(Opnd, VisitedPhis))
         return false;
     return true;
   }
@@ -907,8 +910,10 @@ void ObjCARCOpt::OptimizeIndividualCallImpl(
   LLVM_DEBUG(dbgs() << "Visiting: Class: " << Class << "; " << *Inst << "\n");
 
   // We can delete this call if it takes an inert value.
+  SmallPtrSet<Value *, 1> VisitedPhis;
+
   if (IsNoopOnGlobal(Class))
-    if (isInertARCValue(Inst->getOperand(0))) {
+    if (isInertARCValue(Inst->getOperand(0), VisitedPhis)) {
       if (!Inst->getType()->isVoidTy())
         Inst->replaceAllUsesWith(Inst->getOperand(0));
       Inst->eraseFromParent();

llvm/test/Transforms/ObjCARC/inert-global.ll
@@ -8,6 +8,7 @@
 @.str = private unnamed_addr constant [4 x i8] c"abc\00", section "__TEXT,__cstring,cstring_literals", align 1
 @.str1 = private unnamed_addr constant [4 x i8] c"def\00", section "__TEXT,__cstring,cstring_literals", align 1
 @_unnamed_cfstring_ = private global %struct.__NSConstantString_tag { i32* getelementptr inbounds ([0 x i32], [0 x i32]* @__CFConstantStringClassReference, i32 0, i32 0), i32 1992, i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i64 3 }, section "__DATA,__cfstring", align 8 #0
+@_unnamed_cfstring_.1 = private global %struct.__NSConstantString_tag { i32* getelementptr inbounds ([0 x i32], [0 x i32]* @__CFConstantStringClassReference, i32 0, i32 0), i32 1992, i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str1, i32 0, i32 0), i64 3 }, section "__DATA,__cfstring", align 8 #0
 @_unnamed_cfstring_wo_attr = private global %struct.__NSConstantString_tag { i32* getelementptr inbounds ([0 x i32], [0 x i32]* @__CFConstantStringClassReference, i32 0, i32 0), i32 1992, i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str1, i32 0, i32 0), i64 3 }, section "__DATA,__cfstring", align 8
 @_NSConcreteGlobalBlock = external global i8*
 @.str.1 = private unnamed_addr constant [6 x i8] c"v8@?0\00", align 1
@@ -85,6 +86,40 @@ return:
   %v4 = bitcast %0* %phi2 to i8*
   %v5 = tail call i8* @llvm.objc.autoreleaseReturnValue(i8* %v4)
   ret %0* %phi2
+}
+
+; CHECK-LABEL: define void @test_conditional1(
+; CHECK-NOT: @llvm.objc
+; CHECK: ret void
+
+define void @test_conditional1(i32 %i) {
+entry:
+  %v0 = add nsw i32 %i, -1
+  %c1 = icmp eq i32 %v0, 0
+  br i1 %c1, label %while.end, label %while.body
+
+while.body:
+  %v1 = phi i32 [ %v5, %if.end ], [ %v0, %entry ]
+  %v2 = phi i8* [ %v4, %if.end ], [ bitcast (%struct.__NSConstantString_tag* @_unnamed_cfstring_.1 to i8*), %entry ]
+  %v3 = tail call i8* @llvm.objc.retain(i8* %v2)
+  %cmp = icmp eq i32 %v1, 2
+  br i1 %cmp, label %if.then, label %if.end
+
+if.then:
+  call void @llvm.objc.release(i8* %v2)
+  br label %if.end
+
+if.end:
+  %v4 = phi i8* [ bitcast (%struct.__NSConstantString_tag* @_unnamed_cfstring_ to i8*), %if.then ], [ %v2, %while.body ]
+  call void @llvm.objc.release(i8* %v2)
+  %v5 = add nsw i32 %v1, -1
+  %tobool = icmp eq i32 %v5, 0
+  br i1 %tobool, label %while.end, label %while.body
+
+while.end:
+  %v6 = phi i8* [ null, %entry ], [ %v4, %if.end ]
+  call void @llvm.objc.release(i8* %v6)
+  ret void
 }
 
 declare void @foo()